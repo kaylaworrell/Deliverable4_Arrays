f6ddc65f600b2881ed260073a71b8b4ac7e20a2f
Kayla Worrell <Kayla Worrell@KAYLAS-LAPTOP>
2023-02-09 12:28:29 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Deliverable4_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 25;
+
+            int[] fibonacci = new int[n];
+            fibonacci[0] = 0;
+            fibonacci[1] = 1;
+
+            for (int i = 2; i < n; i++)
+            {
+                fibonacci[i] = fibonacci[i - 2] + fibonacci[i - 1];
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Fibonacci number (" + i + ") = " + fibonacci[i]);
+            }
+        }
+    }
+}
